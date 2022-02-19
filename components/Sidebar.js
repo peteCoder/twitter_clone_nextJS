51d6a9a9fe3f262636d95046db72d44f870e2bdf
Peter Esezobor <70745960+peteCoder@users.noreply.github.com>
2022-02-19 16:03:48 +0100

    second commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 function Sidebar() {
   return (
-      <div>
-        <h1 className="font-sans text-center md:text-left max-w-30 m-auto font-bold text-teal-700 my-2">Angela Davidson</h1>
-        <p className='md:max-w-[60%] text-teal-300 md:mr-auto text-center md:text-left'>
-            This is Angela. She is an orphan. She is in need of financial aid and people to help her get 
-            on her feet.
-        </p>
-        <button className='bg-teal-800 text-white border-teal-500 border-2 inline-block px-4 py-2 rounded mt-5 hover:bg-white hover:text-teal-800 transition-color'>Support</button>
-      </div>
+    <div className="flex m-10 shadow flex-col-reverse max-w-[200px] md:flex-row p-6 relative md:max-w-[540px] mx-auto  overflow-hidden rounded-md">
+        <div>
+            <h1 className="font-sans text-center md:text-left max-w-30 m-auto font-bold text-teal-700 my-2">Angela Davidson</h1>
+            <p className='md:max-w-[60%] text-teal-300 md:mr-auto text-center md:text-left'>
+                This is Angela. She is an orphan. She is in need of financial aid and people to help her get 
+                on her feet.
+            </p>
+            <button className='bg-teal-800 text-white border-teal-500 border-2 inline-block px-4 py-2 rounded mt-5 hover:bg-white hover:text-teal-800 transition-color'>Support</button>
+        </div>
+        <img className='md:object-cover h-[90px] mx-auto max-w-[90px] md:max-w-[200px] rounded-[50%] md:rounded-none md:absolute md:top-0 md:right-0 md:h-full ' src="/child.jpg" alt="baby" width={100} height={100}/>
+    </div>
+      
    
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,9 @@ export default function Home() {
       <Head>
         <title>Love</title>
       </Head>
-      <div className="flex m-10 shadow flex-col-reverse max-w-[200px] md:flex-row p-6 relative md:max-w-[540px] mx-auto  overflow-hidden rounded-md">
-        <Sidebar />
-        <img className='md:object-cover h-[90px] mx-auto max-w-[90px] md:max-w-[200px] rounded-[50%] md:rounded-none md:absolute md:top-0 md:right-0 md:h-full ' src="/child.jpg" alt="baby" width={100} height={100}/>
-      </div>
+      <Sidebar/>
+      <Sidebar/>
+      <Sidebar/>
     </>
     
     
