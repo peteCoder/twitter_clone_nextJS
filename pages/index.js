a731fce950f9e22193d165c5c34fd5709e13b11a
Peter Esezobor <70745960+peteCoder@users.noreply.github.com>
2022-02-19 14:08:40 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl hover:text-teal-300 hover:cursor-pointer font-bold md:flex md:justify-center h-[100vh] md:items-center">
       Hello world!
     </h1>
   )
