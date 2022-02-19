e75845ddf3b8c16d0583e9fac5f918258ce49332
Peter Esezobor <70745960+peteCoder@users.noreply.github.com>
2022-02-19 16:52:09 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
   }
