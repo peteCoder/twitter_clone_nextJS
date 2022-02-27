332f30def6a807421e529396a300e92d781ea92b
Peter Esezobor <70745960+peteCoder@users.noreply.github.com>
2022-02-27 02:27:38 +0100

    committed

--- components/SidebarLink.js
+++ components/SidebarLink.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function SidebarLink({text, Icon, active}) {
   return (
-    <div className={`text-[#d9d9d9] flex items-center justify-center 
+    <div className={`text-[#d9d9d9] flex items-center justify-center
       xl:justify-start text-xl space-x-3 hoverAnimation ${active && 'font-bold'}`}>
-      <Icon className="h-7" />
+      <Icon className="w-6 h-6" />
       <span className='hidden xl:inline'>{text}</span>
     </div>
   )
